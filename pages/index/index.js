9dc8af398ca8c4b2055acec51187527ad79c4b10
ZhangXL-WaHaHa <Xl,19970727>
2020-08-28 23:13:02 +0800

    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,379 +1,140 @@
 //logs.js
-const util = require('../../utils/util.js')
-let aa = {
-  "catList": [{
-      "id": 49,
-      "shopId": 1,
-      "categoryName": "22",
-      "createTime": 1509004117000,
-      "goodsList": []
-    },
-    {
-      "id": 48,
-      "shopId": 1,
-      "categoryName": "6666",
-      "createTime": 1509004096000,
-      "goodsList": []
-    },
-    {
-      "id": 47,
-      "shopId": 1,
-      "categoryName": "牛肉",
-      "createTime": 1509003890000,
-      "goodsList": []
-    },
-    {
-      "id": 46,
-      "shopId": 1,
-      "categoryName": "海鲜",
-      "createTime": 1508403109000,
-      "goodsList": []
-    },
-    {
-      "id": 45,
-      "shopId": 1,
-      "categoryName": "GG",
-      "createTime": 1508402074000,
-      "goodsList": []
-    },
-    {
-      "id": 44,
-      "shopId": 1,
-      "categoryName": "炸药",
-      "createTime": 1508400823000,
-      "goodsList": []
-    },
-    {
-      "id": 41,
-      "shopId": 1,
-      "categoryName": "555555",
-      "createTime": 1507626025000,
-      "goodsList": []
-    },
-    {
-      "id": 40,
-      "shopId": 1,
-      "categoryName": "96969",
-      "createTime": 1507625996000,
-      "goodsList": []
-    },
-    {
-      "id": 39,
-      "shopId": 1,
-      "categoryName": "55",
-      "createTime": 1507625974000,
-      "goodsList": []
-    },
-    {
-      "id": 36,
-      "shopId": 1,
-      "categoryName": "马肉",
-      "createTime": 1505871886000,
-      "goodsList": [{
-        "id": 42,
-        "shopId": 1,
-        "catId": 36,
-        "name": "213",
-        "store": 34,
-        "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-        "price": 23,
-        "disabled": 0
-      }]
-    },
-    {
-      "id": 35,
-      "shopId": 1,
-      "categoryName": "猪头",
-      "createTime": 1505871874000,
-      "goodsList": [{
-        "id": 47,
-        "shopId": 1,
-        "catId": 35,
-        "name": "2",
-        "store": 2,
-        "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-        "price": 2,
-        "disabled": 0
-      }]
-    },
-    {
-      "id": 34,
-      "shopId": 1,
-      "categoryName": "小笼包",
-      "createTime": 1505871796000,
-      "goodsList": [{
-        "id": 44,
-        "shopId": 1,
-        "catId": 34,
-        "name": "213",
-        "store": 5,
-        "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-        "price": 60,
-        "disabled": 0
-      }]
-    },
-    {
-      "id": 33,
-      "shopId": 1,
-      "categoryName": "干果",
-      "createTime": 1505871674000,
-      "goodsList": []
-    },
-    {
-      "id": 32,
-      "shopId": 1,
-      "categoryName": "板砖",
-      "createTime": 1505799519000,
-      "goodsList": []
-    },
-    {
-      "id": 31,
-      "shopId": 1,
-      "categoryName": "炸药",
-      "createTime": 1505799073000,
-      "goodsList": []
-    },
-    {
-      "id": 5,
-      "shopId": 1,
-      "categoryName": "肉",
-      "createTime": 1505791642000,
-      "goodsList": [{
-          "id": 43,
-          "shopId": 1,
-          "catId": 5,
-          "name": "狗腿腊肉",
-          "store": 60,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 88,
-          "disabled": 0
-        },
-        {
-          "id": 52,
-          "shopId": 1,
-          "catId": 5,
-          "name": "麻鸡",
-          "store": 10,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 10,
-          "disabled": 0
-        }
-      ]
-    },
-    {
-      "id": 8,
-      "shopId": 1,
-      "categoryName": "王者农药皮肤",
-      "createTime": 1501229094000,
-      "goodsList": [{
-          "id": 7,
-          "shopId": 1,
-          "catId": 8,
-          "name": "拉不住的亚瑟",
-          "store": 50,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 22.2,
-          "disabled": 0
-        },
-        {
-          "id": 8,
-          "shopId": 1,
-          "catId": 8,
-          "name": "拉不住的德玛西亚",
-          "store": 22,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 22,
-          "disabled": 0
-        }
-      ]
-    },
-    {
-      "id": 7,
-      "shopId": 1,
-      "categoryName": "lol皮肤",
-      "createTime": 1501229086000,
-      "goodsList": [{
-        "id": 6,
-        "shopId": 1,
-        "catId": 7,
-        "name": "德玛西亚",
-        "store": 99,
-        "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-        "price": 99,
-        "disabled": 0
-      }]
-    },
-    {
-      "id": 6,
-      "shopId": 1,
-      "categoryName": "飞机大炮",
-      "createTime": 1501229077000,
-      "goodsList": [{
-        "id": 5,
-        "shopId": 1,
-        "catId": 6,
-        "name": "大刀",
-        "store": 10,
-        "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-        "price": 32.41,
-        "disabled": 0
-      }]
-    },
-    {
-      "id": 4,
-      "shopId": 1,
-      "categoryName": "零食",
-      "createTime": 1500971858000,
-      "goodsList": [{
-          "id": 2,
-          "shopId": 1,
-          "catId": 4,
-          "name": "山泉水",
-          "store": 100,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 0.01,
-          "disabled": 0
-        },
-        {
-          "id": 3,
-          "shopId": 1,
-          "catId": 4,
-          "name": "中华",
-          "store": 100,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 50,
-          "disabled": 0
-        },
-        {
-          "id": 4,
-          "shopId": 1,
-          "catId": 4,
-          "name": "老婆饼",
-          "store": 100,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 100,
-          "disabled": 0
-        }
-      ]
-    },
-    {
-      "id": 1,
-      "shopId": 1,
-      "categoryName": "酒水",
-      "createTime": 1500971822000,
-      "goodsList": [{
-          "id": 1,
-          "shopId": 1,
-          "catId": 1,
-          "name": "矿泉水",
-          "store": 100,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 0.01,
-          "disabled": 1
-        },
-        {
-          "id": 48,
-          "shopId": 1,
-          "catId": 1,
-          "name": "纯生",
-          "store": 800,
-          "image": "http://119.23.56.196:8888/prizeclawspc/resource/images/item/15096878908242 (2).png",
-          "price": 6.5,
-          "disabled": 0
-        }
-      ]
-    }
-  ],
-  "shop": {
-    "id": 1,
-    "name": "起帆棋牌",
-    "avatar": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502800400869&di=a588089ff0d0072abafc3f2ec3c01806&imgtype=0&src=http%3A%2F%2Fp.zhuna.cn%2FHotel_Images%2F33479%2F283b40cb-30d5-403d-b583-d7ace1afd8a0.jpg",
-    "timeStart": 1500543684000,
-    "timeEnd": 1500565289000,
-    "mobile": "13602427535",
-    "status": 2,
-    "areaId": 440100,
-    "addr": "临江大道57号中和广场16A"
-  },
-  "hasFav": 0
-};
+const ballFallAnimation = require('../../utils/ballFallAnimation.js')
 
 var app = getApp()
 Page({
   data: {
-    goodList:[
-      {
-
-      }
-    ],
+    commodityList: [],
     ballAnimationArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], //小球动画
   },
+
+  onLoad: function () {
+    // 获取商品数组
+    for (let i = 0; i < 20; i++) {
+      this.data.commodityList.push({
+        image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598635719606&di=d90965331efd5c6dc2645ecb5fbba324&imgtype=0&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D2905120353%2C3638949522%26fm%3D214%26gp%3D0.jpg',
+        name: '这是商品',
+        price: 99
+      })
+    }
+    this.setData({
+      commodityList: this.data.commodityList
+    })
+
+    // 设置购物车位置
+    this.busPos = {};
+    this.busPos['x'] = 45; //购物车的位置
+    this.busPos['y'] = wx.getSystemInfoSync().windowHeight - 56;
+  },
+
   onReady: function () {
     this.ballComponent = []
     // Do something when page ready.
     // 循环获取所有的小球节点
-    for(let i = 0; i < this.data.ballAnimationArray.length; i++) {
+    for (let i = 0; i < this.data.ballAnimationArray.length; i++) {
       // 获取小球节点信息
       this.ballAnimation = this.selectComponent(`#ball-${i}`)
       // 将小球信息存储
       this.ballComponent.push(this.ballAnimation)
     }
   },
-  onLoad: function (options) {
-    let systemInfo = wx.getStorageSync('systemInfo');
-    let mechine = options; //wx.getStorageSync('mechine');
-    let _that = this;
 
-    this.busPos = {};
-    this.busPos['x'] = 45; //购物车的位置
-    this.busPos['y'] = wx.getSystemInfoSync().windowHeight - 56;
+  tapAdd(e) {
+    let points = ballFallAnimation.touchOnGoods({
+      x: e.touches["0"].clientX - 10,
+      y: e.touches["0"].clientY - 50
+    }, this.busPos, 80)
 
-    this.setData({
-      mechine: mechine,
-      systemInfo: systemInfo,
-      goodsH: systemInfo.windowHeight - 245 - 48
-    });
-
-
-    _that.setData({
-      chessRoomDetail: aa //res.data.Object
-    });
-
-    let timeStart = util.getLocalTime(_that.data.chessRoomDetail.shop.timeStart)
-    let timeEnd = util.getLocalTime(_that.data.chessRoomDetail.shop.timeEnd)
-
-    //初始化右侧商品一开始滚动的位置
-    _that.setData({
-      toView: _that.GOODVIEWID + _that.data.chessRoomDetail.catList[0].id,
-      timeStart: timeStart,
-      timeEnd: timeEnd
-    });
-
-    //存下项目下的产品个数
-    for (let i = 0; i < _that.data.chessRoomDetail.catList.length; i++) {
-      _that.data.goodsNumArr.push(_that.data.chessRoomDetail.catList[i].goodsList.length);
-      let goods = _that.data.chessRoomDetail.catList[i].goodsList;
-      if (goods.length > 0) {
-        for (let j = 0; j < goods.length; j++) {
-          //console.log(goods[j].id)
-          _that.data.goodMap[goods[j].id] = goods[j];
-        }
-      }
-
-    }
-    let HArr = [];
-    for (let j = 0; j < _that.data.goodsNumArr.length; j++) {
-      if (j == 0) {
-        HArr.push(0);
-      } else {
-        HArr.push(_that.data.goodsNumArr[j] * 98 + HArr[j - 1]);
-      }
-    }
-
-    _that.data.goodsNumArr = HArr;
-
+    this.startAnimation(points)
   },
-  onShareAppMessage: function () {
+
+  startAnimation: function (points) {
+    let keyFrames = []
+
+    var index = 0,
+      bezier_points = points['bezier_points'];
+
+    var len = bezier_points.length;
+    index = len
+
+    // 控制多个小球同时出现动画，需要判断上一个动画是否完成
+    // 数组循环，每次开启动画弹出一个数组里面，完成动画之后重新排队
+    let id = this.data.ballAnimationArray.pop()
+    
+    // 放入关键帧
+    for (let i = index - 1; i > -1; i--) {
+      keyFrames.push({
+        opacity: i === 0 ? 0 : 1,
+        left: bezier_points[i]['x'] + 'px',
+        top: bezier_points[i]['y'] + 'px',
+        offset: 0.4
+      })
+    }
+
+    // 创建多个小球节点实现小球的动画
+    // console.log('输出关键帧', keyFrames)
+
+    // // 创建小球动画  
+    // this.animate(`#ball-${id}`, keyFrames, 100, function () {
+    //   this.addGoodToCartFn(e)
+
+    //   // 每次完成动画清空之后放进去  基本可以实现连续小球动画，但是连续点击，小球动画会有明显的卡顿
+    //   // 原因未明
+    //   // 考虑使用小球动画组件  效果基本一致
+
+    //   this.clearAnimation(`#ball-${id}`, () => {
+    //     console.log('经过这一步')
+    //     // 动画完成，将数据重新放入数组中
+    //     this.data.ballAnimationArray.unshift(id)
+
+    //   })
+    // }.bind(this))
+
+    // 开启小球组件动画,连续点击效果也还是会卡顿
+    this.ballComponent[id].startAnimation(keyFrames)
+
+    // 使用小球
+
+    // 
+    // 创建时间间隔
+    // setTimeout(() =>{
+    //   // 创建第二个动画
+    //   this.animate(`#ball-${2}`, keyFrames, 100, function() {
+    //     this.addGoodToCartFn(e)
+
+    //   }.bind(this))
+    // }, 100)
+    // this.timer = setInterval(function () {
+    //   for(let i = index - 1; i > -1; i--) {
+    //     console.log('输出数据', bezier_points[i]['x'])
+    //     that.setData({
+    //       bus_x: bezier_points[i]['x'],
+    //       bus_y: bezier_points[i]['y']
+    //     })
+
+    //     if (i < 1) {
+    //       clearInterval(that.timer);
+    //       that.addGoodToCartFn(e);
+    //       that.setData({
+    //         hide_good_box: true
+    //       })
+    //     }
+    //   }
+    // }, 25);
+  },
+
+  // 小球组件动画结束
+  endAnimation(e) {
+    this.data.ballAnimationArray.unshift(e.detail)
+    
+    // 处理事件逻辑
+  },
+
+
+
+
+  // 
+  areAppMessage: function () {
     let _that = this;
     return {
       title: _that.data.chessRoomDetail.shop.name,
@@ -454,122 +215,6 @@ Page({
     });
 
     this._resetTotalNum();
-  },
-  touchOnGoods: function (e) {
-    this.finger = {};
-    var topPoint = {};
-    this.finger['x'] = e.touches["0"].clientX - 10; //点击的位置，增加偏移位置改变小球出现的位置
-    this.finger['y'] = e.touches["0"].clientY - 50;
-
-    if (this.finger['y'] < this.busPos['y']) {
-      topPoint['y'] = this.finger['y'] - 150;
-    } else {
-      topPoint['y'] = this.busPos['y'] - 150;
-    }
-    topPoint['x'] = Math.abs(this.finger['x'] - this.busPos['x']) / 2;
-
-    if (this.finger['x'] > this.busPos['x']) {
-      topPoint['x'] = (this.finger['x'] - this.busPos['x']) / 2 + this.busPos['x'];
-    } else { //
-      topPoint['x'] = (this.busPos['x'] - this.finger['x']) / 2 + this.finger['x'];
-    }
-
-    this.linePos = app.bezier([this.busPos, topPoint, this.finger], 80);
-    this.startAnimation(e);
-  },
-
-
-  startAnimation: function (e) {
-    let keyFrames = []
-
-    var index = 0,
-      that = this,
-      bezier_points = that.linePos['bezier_points'];
-
-    this.setData({
-      hide_good_box: false,
-      bus_x: that.finger['x'],
-      bus_y: that.finger['y']
-    })
-    var len = bezier_points.length;
-    index = len
-
-    // 控制多个小球同时出现动画，需要判断上一个动画是否完成
-    // 数组循环，每次开启动画弹出一个数组里面，完成动画之后重新排队
-    // 多个动画同时出现,怎么控制是哪一个小球进行动画，小球动画完成之后，又怎么实现重新动画
-    let id = this.data.ballAnimationArray.pop()
-    // console.log('输出弹出的信息', id, this.data.ballAnimationArray)
-    // 使用关键帧动画
-    // 放入关键帧
-    for (let i = index - 1; i > -1; i--) {
-      keyFrames.push({
-        opacity: i === 0 ? 0 : 1,
-        left: bezier_points[i]['x'] + 'px',
-        top: bezier_points[i]['y'] + 'px',
-        offset: 0.4
-      })
-    }
-
-    // 创建多个小球节点实现小球的动画
-    // console.log('输出关键帧', keyFrames)
-
-    // // 创建小球动画  
-    // this.animate(`#ball-${id}`, keyFrames, 100, function () {
-    //   this.addGoodToCartFn(e)
-
-    //   // 每次完成动画清空之后放进去  基本可以实现连续小球动画，但是连续点击，小球动画会有明显的卡顿
-    //   // 原因未明
-    //   // 考虑使用小球动画组件  效果基本一致
-    
-    //   this.clearAnimation(`#ball-${id}`, () => {
-    //     console.log('经过这一步')
-    //     // 动画完成，将数据重新放入数组中
-    //     this.data.ballAnimationArray.unshift(id)
-
-    //     console.log('输出动画完成之后的数组', this.data.ballAnimationArray)
-    //   })
-    // }.bind(this))
-
-    // 开启小球组件动画,连续点击效果也还是会卡顿
-    this.ballComponent[id].startAnimation(keyFrames)
-
-    // 使用小球
-
-    // 
-    // 创建时间间隔
-    // setTimeout(() =>{
-    //   // 创建第二个动画
-    //   this.animate(`#ball-${2}`, keyFrames, 100, function() {
-    //     this.addGoodToCartFn(e)
-
-    //   }.bind(this))
-    // }, 100)
-    // this.timer = setInterval(function () {
-    //   for(let i = index - 1; i > -1; i--) {
-    //     console.log('输出数据', bezier_points[i]['x'])
-    //     that.setData({
-    //       bus_x: bezier_points[i]['x'],
-    //       bus_y: bezier_points[i]['y']
-    //     })
-
-    //     if (i < 1) {
-    //       clearInterval(that.timer);
-    //       that.addGoodToCartFn(e);
-    //       that.setData({
-    //         hide_good_box: true
-    //       })
-    //     }
-    //   }
-    // }, 25);
-  },
-
-  // 小球组件动画结束
-  endAnimation(e) {
-    // 将该小球的id重新推进去
-    this.data.ballAnimationArray.unshift(e.detail)
-
-    // 数量改变
-    // this.addGoodToCartFn()
   },
 
   //移除商品的事件
---
+++ utils/ballFallAnimation.js
@@ -0,0 +1,80 @@
+module.exports = {
+
+  /**
+   * 手指点击
+   * @param {
+   *    e  带手指点击的参数
+   *    busPos   购物车位置
+   *    amount  计算获取点的数目
+   * }
+   * @returns 小球运动轨迹坐标点
+   */
+  touchOnGoods: function (e, busPos, amount) {
+    this.finger = {};
+    var topPoint = {};
+    this.finger['x'] = e.x; //点击的位置，增加偏移位置改变小球出现的位置
+    this.finger['y'] = e.y;
+
+    if (this.finger['y'] < busPos['y']) {
+      topPoint['y'] = this.finger['y'] - 150;
+    } else {
+      topPoint['y'] = busPos['y'] - 150;
+    }
+    topPoint['x'] = Math.abs(this.finger['x'] - busPos['x']) / 2;
+
+    if (this.finger['x'] > busPos['x']) {
+      topPoint['x'] = (this.finger['x'] - busPos['x']) / 2 + busPos['x'];
+    } else { //
+      topPoint['x'] = (busPos['x'] - this.finger['x']) / 2 + this.finger['x'];
+    }
+
+    return this.bezier([busPos, topPoint, this.finger], amount);
+  },
+
+  /**
+   * 获取小球运动的轨迹点
+   * @param 
+   * @returns 
+   */
+  bezier: function (pots, amount) {
+    var pot;
+    var lines;
+    var ret = [];
+    var points;
+    for (var i = 0; i <= amount; i++) {
+      points = pots.slice(0);
+      lines = [];
+      while (pot = points.shift()) {
+        if (points.length) {
+          lines.push(pointLine([pot, points[0]], i / amount));
+        } else if (lines.length > 1) {
+          points = lines;
+          lines = [];
+        } else {
+          break;
+        }
+      }
+      ret.push(lines[0]);
+    }
+    function pointLine(points, rate) {
+      var pointA, pointB, pointDistance, xDistance, yDistance, tan, radian, tmpPointDistance;
+      var ret = [];
+      pointA = points[0];//点击
+      pointB = points[1];//中间
+      xDistance = pointB.x - pointA.x;
+      yDistance = pointB.y - pointA.y;
+      pointDistance = Math.pow(Math.pow(xDistance, 2) + Math.pow(yDistance, 2), 1 / 2);
+      tan = yDistance / xDistance;
+      radian = Math.atan(tan);
+      tmpPointDistance = pointDistance * rate;
+      ret = {
+        x: pointA.x + tmpPointDistance * Math.cos(radian),
+        y: pointA.y + tmpPointDistance * Math.sin(radian)
+      };
+      return ret;
+    }
+    return {
+      'bezier_points': ret
+    };
+  },
+};
