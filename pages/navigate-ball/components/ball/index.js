e07aa62eb5839c72247dea3b9d7f8a8992cd158e
张晓立 <zhangxiaoli@yungeio.com>
2021-02-01 15:35:51 +0800

    更新个球

--- pages/navigate-ball/components/ball/index.js
+++ pages/navigate-ball/components/ball/index.js
@@ -4,7 +4,37 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    /**
+     * 悬浮球一开始出现的位置
+     */
+    left: {
+      type: String,
+      value: '100rpx'
+    },
+    top: {
+      type: String,
+      value: '100rpx'
+    },
 
+    /**
+     * 悬浮球的大小
+     */
+    width: {
+      type: String,
+      value: '80rpx'
+    },
+    height: {
+      type: String,
+      value: '80rpx'
+    },
+
+    /**
+     * 半透明圆环宽度
+     */
+    border_width: {
+      type: String,
+      value: '16rpx'
+    }
   },
 
   /**
